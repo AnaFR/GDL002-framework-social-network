99b70f202fbf634a80a8b79541fd85c8c338594f
unknown <anafrs26@gmail.com>
2019-06-26 12:30:33 -0500

    Updates

---
+++ precache-manifest.ecb933a93e2c80dd94207e47fbe75d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60652f3166077cc21ac02dc92ef30f7a",
+    "url": "/GDL002-framework-social-network/index.html"
+  },
+  {
+    "revision": "d3b222037a44b101f2b3",
+    "url": "/GDL002-framework-social-network/static/css/main.1b398baf.chunk.css"
+  },
+  {
+    "revision": "5009f6b1a804566f5802",
+    "url": "/GDL002-framework-social-network/static/js/2.15bb3818.chunk.js"
+  },
+  {
+    "revision": "d3b222037a44b101f2b3",
+    "url": "/GDL002-framework-social-network/static/js/main.b986936c.chunk.js"
+  },
+  {
+    "revision": "6cead313ad965984df26",
+    "url": "/GDL002-framework-social-network/static/js/runtime~main.33323341.js"
+  }
+]);
